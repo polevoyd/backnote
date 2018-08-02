ff381191194653d53a7102cfba4cffc5aab6dff3
Pablito14 <pablo.rosales14@gmail.com>
2018-08-02 14:46:18 -0700

    Done?

--- js/home.js
+++ js/home.js
@@ -1,31 +1,49 @@
-var signupButton = document.getElementById('submit-button');
-var feedbackData = [];
+var signupForm = document.getElementById('newsletter-form');
 
-
-if (localStorage.getItem('feedbackData')){
-
-
-  feedbackData.JSON.parse(localStorage.getItem('feedbackData'));
-
+//create constructor for newletter item
+function NewsletterItem(name, email, textArea) {
+  this.name = name;
+  this.email = email;
+  this.textArea = textArea;
+  localStorage.setItem(this.email, JSON.stringify(this));
 }
 
-signupButton.addEventListener('submit', hitSignup);
+signupForm.addEventListener('submit', hitSignup);
 
 function hitSignup(e) {
   e.preventDefault();
   var name = document.getElementById('name').value;
   var email = document.getElementById('e-mail').value;
   var textArea = document.getElementById('textarea').value;
-
-  feedbackData.push(name);
-  feedbackData.push(email);
-  feedbackData.push(textArea);
-
-  localStorage.setItem(JSON.stringify(feedbackData));
+  new NewsletterItem(name, email, textArea);
 }
 
+if (localStorage.getItem(this.email)) {
+  JSON.parse(localStorage.getItem(this.email));
+}
 
-// function openPopup(){
+var submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', openPopup);
+var popupElement = document.getElementById('popup');
+// var popupElement = document.createElement('div');
 
-// }
+function openPopup() {
 
+  var name = document.getElementById('name').value;
+  //popup message
+  var msg = '<div class="header"><a id = "close" href="#">X</a></div>';
+  msg += '<div><h3>Thank you!</h3></div>';
+  msg += '<div><img src="images/groupimg.jpg" width ="306px" height ="auto"></div>';
+  msg += '<p>We appreciate your support ' + name + '.<br>' + 'Welcome to the Backnote community!<br>';
+  msg += 'You have been locally stored :-)<p>';
+  popupElement.innerHTML = msg;
+  popupElement.style.border ='1px solid sienna';
+  popupElement.style.backgroundColor = 'white';
+  var closeElement = document.getElementById('close');
+  closeElement.addEventListener('click', closePopup);
+}
+
+function closePopup(e) {
+  e.preventDefault();
+  popupElement.remove();
+}
